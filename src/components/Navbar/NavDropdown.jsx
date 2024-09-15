463581e78b3e64fdaab3bcec4c21a56c257f209a
Falguni Mondal <career.falguni@gmail.com>
2024-09-16 02:08:10 +0530

    Improvements on nav categories section

--- src/components/Navbar/NavDropdown.jsx
+++ src/components/Navbar/NavDropdown.jsx
@@ -11,13 +11,13 @@ const NavDropdown = ({ data, visibility }) => {
   // }
 
   return (
-    <div className={`navcategory-bg fixed ${visible ? 'visible' : 'invisible'} h-[86.8vh] top-[80px] left-0 w-full flex justify-center bg-[rgba(0,0,0,0.1)]`}>
-      <div className="navcategory w-[85%] h-[70vh] bg-white p-4 px-8 rounded-sm">
+    <div className={`navcategory-bg fixed invisible h-[86.8vh] top-[80px] left-0 w-full flex justify-center bg-[rgba(0,0,0,0.1)]`}>
+      <div className="navcategory opacity-0 w-[85%] h-[70vh] bg-white p-4 px-8 rounded-sm">
         <ul className='category-list-container w-full h-full flex flex-col flex-wrap gap-1.5'>
           {
             categories.map(category => (
               <li key={category.cate_path.slice(1, category.cate_path.length)} className='w-1/5 px-2 mb-1.5 leading-none'>
-                <Link className={`${categoryColor} font-bold text-[0.88rem] tracking-tight w-full inline-block pb-1.5`} to={category.cate_path}>
+                <Link className={`${categoryColor} category-link font-bold text-[0.88rem] tracking-tight w-full inline-block pb-1.5`} to={category.cate_path}>
                   {category.cate_title}
                 </Link>
                 {
@@ -27,7 +27,7 @@ const NavDropdown = ({ data, visibility }) => {
                       {
                         category.subCategory.map(subCat => (
                           <li className='w-full leading-none font-[assistant-regular]' key={subCat.subc_path.slice(1, subCat.subc_path.length)}>
-                            <Link className='text-[0.88rem] py-1 w-full inline-block' to={subCat.subc_path}>{subCat.subc_title}</Link>
+                            <Link className='category-link text-[0.88rem] py-1 w-full inline-block' to={subCat.subc_path}>{subCat.subc_title}</Link>
                           </li>
                         ))
                       }
